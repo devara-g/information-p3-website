a64cbefc5c7714bde27a363912a5ecf659d513cd
Devara Hermawan <defarahermawan@gmail.com>
2026-03-19 14:55:02 +0700

    Add files via upload

--- components/header.php
+++ components/header.php
@@ -5,20 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SMP PGRI 3 BOGOR - Sekolah Hebat Indonesia</title>
-    
-    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="stylesheet" href="css/style.css?v=<?= time(); ?>">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link rel="icon" href="img/pgri3-photoroom.png">
+    <link rel="icon" href="img/p3hd.jpg?v=<?= time(); ?>">
 </head>
 
 <body>
-    <header>
+    <header class="floating-header">
         <nav>
             <a href="index.php" class="logo">
-                <img src="img/pgri3-photoroom.png" alt="Logo Sekolah">
+                <img src="img/p3hd.jpg?v=<?= time(); ?>" alt="Logo Sekolah">
                 <span>SMP PGRI 3 BGR</span>
             </a>
 
@@ -29,45 +30,45 @@
             </div>
 
             <ul class="nav-links">
-                <li><a href="index.php"><i class="fas fa-home"></i> Beranda</a></li>
+                <li><a href="index.php">Beranda</a></li>
                 <li class="dropdown">
-                    <a href="#"><i class="fas fa-info-circle"></i> Tentang <i class="fas fa-chevron-down" style="font-size: 0.7rem; margin-left: 4px;"></i></a>
+                    <a href="#">Tentang <i class="fas fa-chevron-down" style="font-size: 0.7rem; margin-left: 4px;"></i></a>
                     <ul class="dropdown-menu">
-                        <li><a href="pages/about.php"><i class="fas fa-school"></i> Profil Sekolah</a></li>
-                        <li><a href="pages/visi-misi.php"><i class="fas fa-bullseye"></i> Visi & Misi</a></li>
+                        <li><a href="pages/about.php">Profil Sekolah</a></li>
+                        <li><a href="pages/visi-misi.php">Visi & Misi</a></li>
                     </ul>
                 </li>
                 <li class="dropdown">
-                    <a href="#"><i class="fas fa-newspaper"></i> Berita <i class="fas fa-chevron-down" style="font-size: 0.7rem; margin-left: 4px;"></i></a>
+                    <a href="#">Berita <i class="fas fa-chevron-down" style="font-size: 0.7rem; margin-left: 4px;"></i></a>
                     <ul class="dropdown-menu">
-                        <li><a href="pages/berita.php"><i class="fas fa-rss"></i> Berita Terbaru</a></li>
-                        <li><a href="pages/agenda.php"><i class="fas fa-calendar-alt"></i> Agenda Sekolah</a></li>
+                        <li><a href="pages/berita.php">Berita Terbaru</a></li>
+                        <li><a href="pages/agenda.php">Agenda Sekolah</a></li>
                     </ul>
                 </li>
                 <li class="dropdown">
-                    <a href="#"><i class="fas fa-images"></i> Galeri <i class="fas fa-chevron-down" style="font-size: 0.7rem; margin-left: 4px;"></i></a>
+                    <a href="#">Galeri <i class="fas fa-chevron-down" style="font-size: 0.7rem; margin-left: 4px;"></i></a>
                     <ul class="dropdown-menu">
-                        <li><a href="pages/galeri.php"><i class="fas fa-camera"></i> Galeri Kegiatan</a></li>
-                        <li><a href="pages/fasilitas.php"><i class="fas fa-building"></i> Fasilitas</a></li>
+                        <li><a href="pages/galeri.php">Galeri Kegiatan</a></li>
+                        <li><a href="pages/fasilitas.php">Fasilitas</a></li>
                     </ul>
                 </li>
                 <li class="dropdown">
-                    <a href="#"><i class="fas fa-sitemap"></i> Struktur <i class="fas fa-chevron-down" style="font-size: 0.7rem; margin-left: 4px;"></i></a>
+                    <a href="#">Struktur <i class="fas fa-chevron-down" style="font-size: 0.7rem; margin-left: 4px;"></i></a>
                     <ul class="dropdown-menu">
-                        <li><a href="pages/struktur.php"><i class="fas fa-user-tie"></i> Kepsek & TU</a></li>
-                        <li><a href="pages/osis.php"><i class="fas fa-users"></i> OSIS</a></li>
-                        <li><a href="pages/mpk.php"><i class="fas fa-user-friends"></i> MPK</a></li>
-                        <li><a href="pages/guru.php"><i class="fas fa-chalkboard-teacher"></i> Guru & Staff</a></li>
+                        <li><a href="pages/kesiswaan.php">Kesiswaan</a></li>
+                        <li><a href="pages/struktur.php">Kepsek & TU</a></li>
+                        <li><a href="pages/osis.php">OSIS</a></li>
+                        <li><a href="pages/mpk.php">MPK</a></li>
+                        <li><a href="pages/guru.php">Guru & Staff</a></li>
                     </ul>
                 </li>
-                <li><a href="pages/kontak.php"><i class="fas fa-envelope"></i> Kontak</a></li>
-                <li><a href="admin/login.php" class="admin-btn"><i class="fas fa-user-shield"></i> Admin</a></li>
+                <li><a href="pages/kontak.php">Kontak</a></li>
             </ul>
         </nav>
     </header>
 
     <a href="admin/login.php" class="admin-fixed" title="Admin Panel">
-        <i class="fas fa-cog"></i>
+        <i class="bx bx-cog"></i>
     </a>
 
-    <script src="js/main.js"></script>
+    <script src="js/main.js?v=<?= time(); ?>"></script>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include 'components/header.php'; ?>
 
 <section class="hero">
-    <div class="floating-shapes">
+
         <div class="shape shape-1"></div>
         <div class="shape shape-2"></div>
         <div class="shape shape-3"></div>
@@ -17,7 +17,7 @@
             <div class="principal-bubble">
                 <i class="fas fa-quote-left quote-icon"></i>
                 <h3>"Pendidikan adalah passport masa depan"</h3>
-                <p>- Drs. Indra Robriandri, M.Si -</p>
+                <p>- Indra Robriandri, S.Pd -</p>
             </div>
         </div>
 
@@ -27,6 +27,7 @@
             </div>
         </div>
     </div>
+    <?php include 'pages/wave.php'; ?>
 </section>
 
 <section class="sambutan-section">
@@ -42,10 +43,10 @@
             <p>Segala puji bagi Allah SWT yang telah memberikan rahmat dan hidayahnya sehingga kita dapat berkumpul di sini. Saya ucapkan selamat datang di website resmi SMP PGRI 3 BOGOR.</p>
             <p>Kami berkomitmen untuk memberikan pendidikan terbaik bagi putra-putri Anda, mencetak generasi yang tidak hanya cerdas secara intelektual, tetapi juga memiliki karakter yang kuat dan berakhlak mulia.</p>
             <p>Sekolah kami berkomitmen untuk memberikan pendidikan terbaik bagi seluruh siswa. Dengan didukung oleh tenaga pengajar yang profesional dan fasilitas yang memadai, kami yakin akan menghasilkan lulusan yang kompeten dan berakhlak mulia.</p>
-            <p>Mari bersama kita tingkatkan kualitas pendidikan dan mewujudkan generasi yang cerdas, kreatif, dan ber创新.</p>
+            <p>Mari bersama kita tingkatkan kualitas pendidikan dan mewujudkan generasi yang cerdas, kreatif, dan berinovasi.</p>
             <p>Wassalamu'alaikum Wr. Wb.</p>
             <div class="ttd">
-                <p>Jakarta, Februari 2026</p>
+                <p>Bogor, Februari 2026</p>
                 <p>Drs. Indra Robriandri, M.Si</p>
             </div>
         </div>
